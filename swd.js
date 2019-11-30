ae66635f08bbb5801a5eecf4ad40df7220439d12
Antony Pyatkov <tltary@yandex.ru>
2019-11-30 21:44:17 +0400

    Update swd.js

--- swd.js
+++ swd.js
@@ -1,7 +1,8 @@
-let div = document.createElement('div');
+var div = document.createElement('div');
 div.style.overflowY = 'scroll';
 div.style.width = '50px';
 div.style.height = '50px';
 document.body.append(div);
-export const swd = div.offsetWidth - div.clientWidth;
+var swd = div.offsetWidth - div.clientWidth
 div.remove();
+return swd;
