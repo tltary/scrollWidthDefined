e02962c556b73821a44cf696b925a5febb2736f1
Antony Pyatkov <tltary@yandex.ru>
2019-11-30 21:50:16 +0400

    return
    return ol version

--- swd.js
+++ swd.js
@@ -1,8 +1,7 @@
-var div = document.createElement('div');
+let div = document.createElement('div');
 div.style.overflowY = 'scroll';
 div.style.width = '50px';
 div.style.height = '50px';
 document.body.append(div);
-var swd = div.offsetWidth - div.clientWidth
+export const swd = div.offsetWidth - div.clientWidth;
 div.remove();
-return swd;
